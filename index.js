089c9268982bb604064c04102ac674dfbcaf9a6f
Manix <admin@manix.info>
2018-02-08 22:45:02 +0200

    added more stats and styles

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const fs = require("fs");
 const JSON = require("circular-json");
+const ua = require("ua-parser-js");
 
-module.exports = function (base) {
+let config = {};
+
+config.parseUser = user => user;
+config.parseConnection = connection => ({
+  id: connection.id,
+  bytesReceived: connection.bytesReceived,
+  device: ua(connection.upgradeReq.headers["user-agent"])
+});
+
+module.exports = function (base, _config = {}) {
+  config = Object.assign(config, _config);
+
   return function (req, res) {
     switch (req.query.route) {
+      case "style":
+        res.set('Content-Type', 'text/css');
+        // intentionally omit break, send file below
       case "file":
         sendFile(req.query.file, res);
         break;
@@ -11,7 +26,18 @@ module.exports = function (base) {
       case "stats":
         res.send(JSON.stringify({
           connections: Object.keys(base.clients.all()).length,
-          users: base.users.all()
+          users: Object.entries(base.users.all()).map(([id, user]) => {
+            let copy = config.parseUser(Object.assign({}, user));
+            
+            copy.connections = Object.entries(user.connections).map(([id, connection]) => {
+              return config.parseConnection(Object.assign({}, connection));
+            });
+
+            return copy;
+          }),
+          memory: process.memoryUsage(),
+          processor: process.cpuUsage(),
+          uptime: process.uptime()
         }))
         break;
 
@@ -24,4 +50,18 @@ module.exports = function (base) {
 
 function sendFile(file, res) {
   res.send(fs.readFileSync(__dirname + "/src/client/" + file.replace("/", "")).toString());
+}
+
+function cleanHiddenProperties(object, copy = true) {
+  if (copy) {
+    object = Object.assign({}, object);
+  }
+
+  for (var key in object) {
+    if (key[0] === "_") {
+      delete(object[key]);
+    }
+  }
+
+  return object;
 }
--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,64 @@
-module = {};
+const toggle = function (event) {
+  this.classList.toggle("expanded");
+
+  event.stopPropagation();
+}
+
+const construct = function (data) {
+  if (Array.isArray(data)) {
+    const table = document.createElement("table");
+    table.classList.add("expanded", "array");
+
+    data.forEach(entry => {
+      const row = document.createElement("tr");
+      const col = document.createElement("td");
+
+      col.appendChild(construct(entry));
+      row.appendChild(col);
+      table.appendChild(row);
+    });
+
+    return table;
+  } else if (typeof data === "object") {
+    const table = document.createElement("table");
+    table.onclick = toggle;
+
+    for (let key in data) {
+      const row = document.createElement("tr");
+      const col1 = document.createElement("td");
+      const col2 = document.createElement("td");
+
+      col1.innerHTML = key;
+      col2.appendChild(construct(data[key]));
+      row.appendChild(col1);
+      row.appendChild(col2);
+      table.appendChild(row);
+    }
+
+    return table;
+  } else {
+    return document.createTextNode(data);
+  }
+}
 
 window.onload = function () {
   document.getElementById("date").innerHTML = new Date().toLocaleString();
 
   fetch("?route=stats").then(response => response.json()).then(json => {
-    const JSONFormatter = module.exports.default;
-    const formatter = new JSONFormatter(json.users);
+    document.getElementById("uptime").innerHTML = json.uptime;
 
-    document.body.appendChild(formatter.render());
+    document.getElementById("users-connections").innerHTML = json.connections;
+    document.getElementById("users-total").innerHTML = Object.keys(json.users).length;
 
-    document.getElementById("connections").innerHTML = json.connections;
-    document.getElementById("users").innerHTML = Object.keys(json.users).length;
-  })
+    const used = json.memory;
+    for (let key in used) {
+      document.getElementById("mem-" + key).innerHTML = `${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`;
+    }
+
+    for (let key in json.processor) {
+      document.getElementById("cpu-" + key).innerHTML = json.processor[key];
+    }
+
+    document.getElementById("table").appendChild(construct(json.users));
+  });
 }
